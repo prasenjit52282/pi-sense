02d5e01ec37b5d17e12bbd289b515eb2e14ad352
SMR_LAB <smrlab@SMRL-iMac.local>
2022-10-18 20:42:44 +0530

    dust sensor reader is added

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,3 @@
 from .co2_sensor import CO2
 from .multi_channel_gas_sensor import GAS
+from .dust_sensor import DUST
---
+++ modules/dust_sensor.py
@@ -0,0 +1,61 @@
+import serial
+
+class DUST:
+    def __init__(self,port='/dev/ttyS0',baudrate=9600):
+        self.port=port
+        self.baudrate=baudrate
+        self.setup()
+
+    def setup(self):
+        self.read_buffer=None
+        self.serial=serial.Serial(self.port,self.baudrate,timeout=1)
+        self.temp,self.hum,self.fmhds,self.pm2_5=None,None,None,None
+
+    def __del__(self):
+        self.serial.close()
+
+    def notifyNextRead(self):
+        self.serial.flushInput()
+
+    def waitingFor40Byte(self):
+        while(self.serial.in_waiting<40):pass
+
+    def readBuff(self):
+        self.read_buffer=self.serial.read(40)
+
+    def decodeReadBuff(self):
+        PMS=0;FMHDS=0;TPS=0;HDS=0;CR2=0
+        CR1=(self.read_buffer[38]<<8)+self.read_buffer[39]
+        for i in range(38):CR2+=self.read_buffer[i]
+        if CR1==CR2:
+            PMSa=self.read_buffer[12];           #Read PM2.5 High 8-bit
+            PMSb=self.read_buffer[13];           #Read PM2.5 Low 8-bit
+            PMS=(PMSa<<8)+PMSb;                  #PM2.5 value
+            FMHDSa=self.read_buffer[28];         #Read Formaldehyde High 8-bit
+            FMHDSb=self.read_buffer[29];         #Read Formaldehyde Low 8-bit
+            FMHDS=(FMHDSa<<8)+FMHDSb;            #Formaldehyde value
+            TPSa=self.read_buffer[30];           #Read Temperature High 8-bit
+            TPSb=self.read_buffer[31];           #Read Temperature Low 8-bit
+            TPS=(TPSa<<8)+TPSb;                  #Temperature value
+            HDSa=self.read_buffer[32];           #Read Humidity High 8-bit
+            HDSb=self.read_buffer[33];           #Read Humidity Low 8-bit
+            HDS=(HDSa<<8)+HDSb;                  #Humidity value
+        self.temp,self.hum,self.fmhds,self.pm2_5=TPS/10.0,HDS/10.0,FMHDS,PMS
+
+    def sample(self):
+        self.notifyNextRead()
+        self.waitingFor40Byte()
+        self.readBuff()
+        self.decodeReadBuff()
+
+    def readTEMP(self):
+        return self.temp
+
+    def readHUM(self):
+        return self.hum
+    
+    def readFMHDS(self):
+        return self.fmhds
+
+    def readPM2_5(self):
+        return self.pm2_5
